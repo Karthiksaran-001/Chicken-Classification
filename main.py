277245378d302ceb09039265d80205c36bd91969
Karthik <mailmekarthik001@gmail.com>
2023-06-11 12:14:27 +0530

    Satge 03 Training Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNNClassifier.pipeline.stage_03_training_pipeline import ModelTrainingPipeline
 from CNNClassifier import logger
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -23,6 +24,18 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
 
 
 
